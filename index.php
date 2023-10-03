384d360d87aab76288ecc61367b9b50e66af7f26
VaibhavVinayakChaudhari <134763341+VaibhavVinayakChaudhari@users.noreply.github.com>
2023-10-03 14:51:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<h1>My first PHP page version my Webhooks and build should start automatically and it works!!!! </h1>
+<h1>My first PHP page version my Webhooks </h1>
 
 <?php
 echo "hello World from test FROM S2I"
