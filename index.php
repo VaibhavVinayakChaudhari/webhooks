8dc12976a151057c63773f3909e5fbd3f5100832
VaibhavVinayakChaudhari <134763341+VaibhavVinayakChaudhari@users.noreply.github.com>
2023-10-03 17:48:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 
-<h1>My first PHP page version my Webhooks  </h1>
+<h1>My first PHP page version my Webhooks and now time for auto update and its works!!! </h1>
 
 <?php
 echo "hello World from test FROM S2I"
